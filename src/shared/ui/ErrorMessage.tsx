50e85ad32aa631fac2c4b4b88269979496a65ea6
noty-trreeeD <noty.trreeed@gmail.com>
2026-05-13 23:58:27 +0600

    style: add red color to error message

--- src/shared/ui/ErrorMessage.tsx
+++ src/shared/ui/ErrorMessage.tsx
@@ -1,11 +1,10 @@
 import {Alert} from "@mui/material"
-
 interface ErrorMessageProps {
     error?: string | null
 }
 
 export const ErrorMessage = ({error}: ErrorMessageProps) => {
     return (
-        <Alert>{error}</Alert>
+        <Alert color={"error"}>{error}</Alert>
     )
 }
